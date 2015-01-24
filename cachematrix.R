32730cda8530fa5db0fb34b1aebdb4c0cd0de436
Sandeep Singhal <sandeepsinghal@gmail.com>
2015-01-24 19:31:36 +0530

    adding few more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# This function exposes functions to return the original matrix 
+# It contains a variable called xInv that will act 
 makeCacheMatrix <- function(x = matrix()) {
     
     # Inverse of X
@@ -39,7 +36,7 @@ cacheSolve <- function(x, ...) {
     
     i <- x$getInverse()
     
-    # If the cache is null, then compute the matrix
+    # If the cache is null, then compute the matrix and set the cache for retrieval next time
     if (is.null(i)){
         print("Cache Empty, solving the matrix : ")
         data <- x$get()
